7fed3956503183d8cb0afd1269ac62732c0a1213
MauroBorzi <mauroborzi91@gmail.com>
2025-07-07 16:07:36 +0200

    [add]evenOdd

--- js/script.js
+++ js/script.js
@@ -1,23 +1,48 @@
-console.log(`-----Pali-----`)
+// console.log(`-----Pali-----`)
+
+// // definizione variabili
+// const userWord = prompt(`Inserisci una parola`)
+// console.log(`Parola inserita: '${userWord}'`)
+
+// const reverseString = reverseStrings(userWord)
+
+
+// // funzione per capovolgere la parola
+// function reverseStrings(text) {
+//   return text.split('').reverse().join('')
+// }
+
+// console.log(`Parola capovolta: '${reverseString}'`)
+
+// // definiamo quando una parola è palindroma
+// if(userWord === reverseString){
+//   console.log(`La parola '${userWord}' è uguale alla parola '${reverseString}'. Quindi la parola scelta è palindroma.`)
+// }
+// else{
+//   console.log(`La parola '${userWord}' è diversa dalla parola '${reverseString}'. Quindi la parola scelta non è palindroma.`)
+// }
+
+
+
+console.log(`-----Pari e Dispari-----`)
 
 // definizione variabili
-const userWord = prompt(`Inserisci una parola`)
-console.log(`Parola inserita: '${userWord}'`)
+const userEvenOdd = prompt(`Scegli tra pari e dispari`)
+const userNumber = parseInt(prompt(`Scegli un numero tra 1 e 5`))
+const pcNumber = Math.floor(Math.random() * 5) + 1
+let even = `pari`
+let odd = `dispari`
 
-const reverseString = reverseStrings(userWord)
+console.log(userEvenOdd, userNumber, pcNumber)
 
-
-// funzione per capovolgere la parola
-function reverseStrings(text) {
-  return text.split('').reverse().join('')
-}
-
-console.log(`Parola capovolta: '${reverseString}'`)
-
-// definiamo quando una parola è palindroma
-if(userWord === reverseString){
-  console.log(`La parola '${userWord}' è uguale alla parola '${reverseString}'. Quindi la parola scelta è palindroma.`)
+function somma(){
+  const sum = userNumber + pcNumber
+if(sum % 2 === 0){
+console.log(`la somma è ${even}`)
 }
 else{
-  console.log(`La parola '${userWord}' è diversa dalla parola '${reverseString}'. Quindi la parola scelta non è palindroma.`)
+  console.log(`la somma è ${odd}`)
 }
+}
+
+somma()
